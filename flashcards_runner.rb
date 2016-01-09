58468cb818c905375bb20cbc52a708f8fdee3172
Rachel Warbelow <rachelwarbelow@gmail.com>
2016-01-08 20:54:19 -0700

    prompt for new file if file does not exist

--- flashcards_runner.rb
+++ flashcards_runner.rb
@@ -2,8 +2,12 @@ require_relative 'round'
 require_relative 'card_generator'
 
 print "Welcome! Enter the name of the file you'd like to use for your deck: "
-
 file = gets.chomp
+
+until File.exist?(file)
+  print "Sorry. That file doesn't exist. Please enter a new file (for example: cards.csv): "
+  file = gets.chomp
+end
 
 cards = CardGenerator.new(file).create_cards
 
